1f15f16b8230e5b46eb48e521ddeb803c77c7eae
dfp <davis.f.peck@protonmail.com>
2018-05-14 08:23:32 -0500

    Added indexes and backrefs

--- primedb.py
+++ primedb.py
@@ -22,16 +22,19 @@ class ItemType (DataModel):
     pass
 
 class Item (DataModel):
-    type_ = ForeignKeyField(ItemType)
-    ducats = 
+    type_ = ForeignKeyField(ItemType, backref='items')
+    # ducats = IntegerField(default=0)
 
 class RelicTier (DataModel):
-    ordinal = SmallIntegerField()
+    ordinal = SmallIntegerField(unique=True)
 
 class Relic (DataModel):
     tier = ForeignKeyField(RelicTier)
     code = CharField(max_length=2)
     vaulted = BooleanField(default=False)
+
+    class Meta:
+        indexes = ((('tier', 'code'), True))
 
     @property
     def name (self):
@@ -46,31 +49,37 @@ class Relic (DataModel):
 
 # Relation Tables #
 class BuildRequirement (RelationModel):
-    needs = ForeignKeyField(Item)
-    build = ForeignKeyField(Item)
+    needs = ForeignKeyField(Item, backref='products')
+    builds = ForeignKeyField(Item, backref='components')
+    need_count = IntegerField(default=1)
+    build_count = IntegerField(default=1)
 
 class Containment (RelationModel):
-    contains = ForeignKeyField(Item)
-    inside = ForeignKeyField(Relic)
+    contains = ForeignKeyField(Item, backref='relics')
+    inside = ForeignKeyField(Relic, backref='contents')
 
 # class Drop (RelationModel):
 #     drops = ForeignKeyField(Relic)
 #     location = ForeignKeyField(Mission)
 
 
-# Setup Code #
-def open ():
-    needs_setup = not os.path.isfile(DB_PATH)
-    _primedb.connect()
-    if needs_setup: setup()
-
+# Initialization Code #
 def setup ():
-    _primedb.create_tables([ItemType, Item, RelicTier, Relic, MissionSector, Mission])
+    _primedb.create_tables([ItemType, Item, RelicTier, Relic])
 
     RelicTier(name='Lith', ordinal=0).save()
     RelicTier(name='Meso', ordinal=1).save()
     RelicTier(name='Neo',  ordinal=2).save()
     RelicTier(name='Axi',  ordinal=3).save()
 
+def open ():
+    needs_setup = not os.path.isfile(DB_PATH)
+    _primedb.connect()
+    if needs_setup: setup()
+
 def close ():
     _primedb.close()
+
+
+# Population Code #
+def populate
