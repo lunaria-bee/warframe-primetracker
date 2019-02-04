4b761d8901296c686313e293e684a858e9d3ad72
dfpeck <davis.f.peck@protonmail.com>
2019-02-03 18:30:24 -0600

    Replaced BuildRequirement's __repr__ w/ __str__

--- primedb.py
+++ primedb.py
@@ -89,8 +89,8 @@ class BuildRequirement (RelationModel):
     class Meta:
         indexes = ( (('needs', 'builds'), True), )
 
-    def __repr__ (self):
-        return "<BuildRequirement: {} <- {}>".format(self.needs, self.builds)
+    def __str__ (self):
+        return "{} <- {}".format(self.needs, self.builds)
 
 class Containment (RelationModel):
     contains = ForeignKeyField(Item, backref='containments')
