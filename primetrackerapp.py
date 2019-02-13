b79a37e1f7273a733fd57ef8e294de1593759f79
davis <davis.f.peck@protonmail.com>
2019-02-13 11:44:38 -0600

    Inventory initialization cycles through part list

--- primetrackerapp.py
+++ primetrackerapp.py
@@ -4,9 +4,19 @@ import kivy
 import primedb as db
 
 from kivy.app import App
+from kivy.uix.popup import Popup
 
 class PrimeTrackerApp (App):
     pass
+
+class InventoryInitPopup (Popup):
+    def parts_init (self):
+        self.parts = db.Item.select_all_products() + db.Item.select_all_components()
+        self.next_part()
+
+    def next_part (self):
+        self.current_part = self.parts.pop(0)
+        self.prime_prompt.text = "Do you have {}?".format(self.current_part.name)
 
 def main ():
     PrimeTrackerApp().run()
