85d102770bfaf5bf7ffc2972dc91f8c55b4b2d2e
davis <davis.f.peck@protonmail.com>
2019-02-13 12:35:18 -0600

    Started DB population UI

--- primetrackerapp.py
+++ primetrackerapp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import kivy
+import kivy, certifi, urllib3
 import primedb as db
 
 from kivy.app import App
@@ -8,6 +8,21 @@ from kivy.uix.popup import Popup
 
 class PrimeTrackerApp (App):
     pass
+
+class DbPopulatePopup (Popup):
+    def execute (self):
+        http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+        table = db.get_relic_drop_table(http)
+        self.bar.max = len(table)
+        for row in table:
+            db.process_relic_drop_table_row(row, http)
+            self.bar.value += 1
+        db.calculate_required_part_quantities()
+        self.dismiss()
+
+    def update_bar (self):
+        self.bar.value += 1
+        
 
 class InventoryInitPopup (Popup):
     def parts_init (self):
