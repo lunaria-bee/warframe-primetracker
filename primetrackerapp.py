a13e0b0471e8f22f9a7ef4795190e405e5277980
davis <davis.f.peck@protonmail.com>
2019-02-13 11:05:16 -0600

    Returned to Kivy development

---
+++ primetrackerapp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import kivy
+import primedb as db
+
+from kivy.app import App
+
+class PrimeTrackerApp (App):
+    pass
+
+def main ():
+    PrimeTrackerApp().run()
+
+if __name__ == '__main__': main()
