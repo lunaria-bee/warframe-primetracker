17d4ded614aab09b7344476fb49738f7201c4932
davis <davis.f.peck@protonmail.com>
2019-02-14 22:10:58 -0600

    Got DB population progress bar working

--- primetrackerapp.py
+++ primetrackerapp.py
@@ -4,40 +4,32 @@ import kivy, certifi, urllib3
 import primedb as db
 
 from threading import Thread
+from functools import partial
 
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.uix.popup import Popup
 
 class PrimeTrackerApp (App):
     pass
 
-class ProgressPopup (Popup):
-    class execution (Thread):
-        def __init__ (self, function, update_bar, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.function = function
-            self.update_bar = update_bar
-
-        def run (self):
-            self.function()
-
-    def update_bar (self, count=1):
-        self.bar.value += count
-
 class DbPopulatePopup (Popup):
-    def execute (self):
-        http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
-        table = db.get_relic_drop_table(http)
-        self.bar.max = len(table)
-        for row in table:
-            db.process_relic_drop_table_row(row, http)
-            self.bar.value += 1
+    def start (self):
+        self.http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+        self.table = db.get_relic_drop_table(self.http)
+        self.bar.max = len(self.table)
+        # self.updater = Clock.schedule_interval(partial(DbPopulatePopup.update, self), 0.5)
+        Thread(target=partial(DbPopulatePopup.populate, self)).start()
+
+    def populate (self):
+        for row in self.table:
+            db.process_relic_drop_table_row(row, self.http)
+            Clock.schedule_once(lambda _: self.update())
         db.calculate_required_part_quantities()
         self.dismiss()
 
-    def update_bar (self):
+    def update (self):
         self.bar.value += 1
-        
 
 class InventoryInitPopup (Popup):
     def parts_init (self):
