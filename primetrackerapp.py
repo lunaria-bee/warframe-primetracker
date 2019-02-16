c1211e37cf8da99ec83469785e6745df80efdc42
davis <davis.f.peck@protonmail.com>
2019-02-15 18:46:25 -0600

    Work on integrating ProgressPopup

--- primetrackerapp.py
+++ primetrackerapp.py
@@ -9,60 +9,69 @@ from functools import partial
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.popup import Popup
+from kivy.logger import Logger
 
 class PrimeTrackerApp (App):
     pass
 
 class ProgressPopup (Popup):
+    # TODO update to display phase info
+
     def __init__ (self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.current_phase_max = 0
+        self.current_phase_percent = 0
         self.cumulative_max = 0
         self.current_phase_value = 0
-        self.description_message = self.description.text
+        self.description_message = self.step_info.text
 
     def new_phase (self, phase_max, phase_percent, message=None):
         percent = self.bar.value_normalized
         self.bar.max = int((self.cumulative_max + phase_max) / (percent + phase_percent))
         self.bar.value = int(self.bar.max * percent)
         self.current_phase_max = phase_max
+        self.current_phase_percent = phase_percent
         self.cumulative_max += phase_max
         self.current_phase_value = 0
-        self.update_description(message)
+        # TODO update phase info
 
     def update_bar (self, steps=1, message=None):
-        self.bar.value += steps
+        self.bar.value += (steps / self.current_phase_max) * self.current_phase_percent * self.bar.max
         self.current_phase_value += steps
-        self.update_description(description)
+        # TODO update step info
+
+    def update_phase_info (self, message=None):
+        pass
+
+    def update_step_info (self, message=None):
+        pass
 
     def update_description(self, message=None):
         if not message is None: self.description_message = message
-        self.description.text = ("{} {(:.0f}/{:.0f})..."
-                                 .format(self.description_message,
-                                         self.current_phase_value,
-                                         self.current_phase_max))
+        self.step_info.text = ("{} ({:.0f}/{:.0f}, {:.0%})..."
+                               .format(self.description_message,
+                                       self.current_phase_value,
+                                       self.current_phase_max,
+                                       self.current_phase_value / self.current_phase_max))
 
-class DbPopulatePopup (Popup):
+class DbPopulatePopup (ProgressPopup):
     def start (self):
-        self.http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
-        self.table = db.get_relic_drop_table(self.http)
-        self.bar.max = len(self.table)
-        # self.updater = Clock.schedule_interval(partial(DbPopulatePopup.update, self), 0.5)
         self.execution = Thread(target=partial(DbPopulatePopup.populate, self)).start()
 
     def populate (self):
-        for row in self.table:
-            db.process_relic_drop_table_row(row, self.http)
-            Clock.schedule_once(lambda _: self.update("Processing Relic drops"))
+        http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+        table = db.get_relic_drop_table(http)
+        Clock.schedule_once(lambda _: self.new_phase(len(table), .9, "Processing Relic drops"))
+        for row in table:
+            db.process_relic_drop_table_row(row, http)
+            Clock.schedule_once(lambda _: self.update_bar())
+
+        products = db.Item.select_all_products()
+        Clock.schedule_once(lambda _: self.new_phase(len(products), .1,  "Processing build requirements"))
         for product in db.Item.select_all_products():
             db.calculate_product_requirement_quantities(product)
-            Clock.schedule_once(lambda _: self.update("Processing build requirements"))
+            Clock.schedule_once(lambda _: self.update_bar())
         self.dismiss()
-
-    def update (self, description):
-        self.bar.value += 1
-        self.description.text = ("{} ({:.0f}/{:.0f})..."
-                                 .format(description, self.bar.value, self.bar.max))
 
 class InventoryInitPopup (Popup):
     def parts_init (self):
